dbda807a958bbbf05fe0a8144799073c051a3e19
hayderalwaeli3-ui <hayderalwaeli3@gmail.com>
2026-05-15 03:21:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -125,7 +125,9 @@ with st.sidebar:
         📞 <b>رقم الهاتف (تفعيل):</b><br> 07879974395
     </div>
     """, unsafe_allow_html=True)
-    
+if "auth" in st.session_state:
+            st.write(f"🎟️ **الكود المفعل:** `{st.session_state.code}`")
+            
        # --- [إضافة خيار الإدارة] ---
         if st.session_state.code == "HAYDER_2026":
             # إعادة إظهار السلايد الجانبي والشريط العلوي للمدير فقط
